e7519a59819c661b1ad9b23dbd7a59d719ffb35a
Antti Lappalainen <antti@prototyper.tech>
2021-01-28 21:56:14 +0200

    Added field UPDATE/PATH for a number field

--- js/util/FileCache.js
+++ js/util/FileCache.js
@@ -321,11 +321,63 @@ class FileCache
      * Authorization header with bearer and token from storage
      * 
      * @param  {string}  route
-     * @param  {string}  path
-     * @param  {string}  field
+     * @param  {string}  fieldKey
+     * @param  {string}  fieldValue
+     * @param  {string}  idKey
+     * @param  {number}  idValue
      * @return {Promise} response
      */
     static async putFieldStringById(route, fieldKey, fieldValue, idKey, idValue)
+    {
+        const bearer = `Bearer ${FileCache.getToken()}`;
+        console.log(`HTTP PUT Authorization: ${bearer}`);
+
+        // ------------------------------------
+        // - Generate multipart payload
+        // ------------------------------------
+
+        const 
+        formData = new FormData();
+        formData.append(fieldKey, fieldValue);
+        formData.append(idKey, idValue);
+
+        const response = await fetch
+        (
+            `${route}/${fieldKey}`,
+            {
+                method: 'PUT',
+                credentials: 'include',
+                headers: 
+                {
+                    'Authorization' : bearer
+                },
+                body: formData
+            }
+        );
+
+        const text = await response.text();
+
+        // -----------------------------------
+        // - Clear route cache
+        // -----------------------------------
+
+        FileCache.clearCache(route);
+
+        return text;
+    }
+
+    /**
+     * Performs a HTTP PUT Request, includes an 
+     * Authorization header with bearer and token from storage
+     * 
+     * @param  {string}  route
+     * @param  {string}  fieldKey
+     * @param  {number}  fieldValue
+     * @param  {string}  idKey
+     * @param  {number}  idValue
+     * @return {Promise} response
+     */
+    static async putFieldNumberById(route, fieldKey, fieldValue, idKey, idValue)
     {
         const bearer = `Bearer ${FileCache.getToken()}`;
         console.log(`HTTP PUT Authorization: ${bearer}`);
