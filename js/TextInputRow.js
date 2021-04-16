b413717e9be40cc36f8c5caa3001ada7f5c082fa
Antti Lappalainen <antti@prototyper.tech>
2021-04-16 15:49:31 +0300

    Stylings moved into components.css, borders set to red tint only when notifier method called

--- js/TextInputRow.js
+++ js/TextInputRow.js
@@ -13,6 +13,8 @@ class TextInputRow extends WCBase
         // - Setup member properties
         // -----------------------------------------------
 
+        this.mNotifyFlag = false;
+
         let required = false;
 
         if (this.hasAttribute('required')) required = true;
@@ -23,54 +25,51 @@ class TextInputRow extends WCBase
         // -----------------------------------------------
 
         this.attachShadow({mode : "open"});
-        this.setupStyle
-        (`.row {
-            display: flex;
-            justify-content: space-between;
-            height: ${props.uploader_row_height};
-            padding: ${props.uploader_row_pad};
-            border-bottom: 1px solid ${props.lightgrey};
-        }
-        .row__label {
-            width: ${props.row_label_width}; 
-            font-size: ${props.text_font_size};
-            font-weight: 200;
-            color: #222;
-            align-self: center;
-        }
-        .row__input {
-            height: ${props.row_input_height};
-            border: 1px solid ${props.grey};
-            padding: 4px;
-            border-radius: 2px;
-            font-weight: 200;
-            color: #222;
-            box-shadow: 0 1px 6px 1px rgba(0,0,0,0.1);
-        }
-        .row__input:focus {
-            outline: none;
-            border: 2px solid ${props.darkgrey};
-        }
-        .row__input:invalid {
-            border: 2px solid ${props.red};
-            background-image: url('assets/icon_asterisk.svg');
-            background-repeat: no-repeat;
-            background-position-x: right;
-        }
-        `);
+
 
         this.setupTemplate
         (`<link rel='stylesheet' href='assets/css/components.css'>
-          <div class='row'>
-            <div class='row__label'><slot></div>
-            <input type='text' class='row__input' ${required ? 'required' : ''}>
-        </div>`);
+          <div class='component'>
+            <div class='component__row'>
+              <div class='component__label'><slot></div>
+              <div class='component__img--required'></div>
+            </div>
+            <input type='text' class='component__input' ${required ? 'required' : ''}>
+         </div>`);
 
-        // ---------------------------
-        // - Grab the input
-        // ---------------------------
+        // -----------------------------------------------------
+        // - Grab the input and the required asterisk div
+        // -----------------------------------------------------
 
-        this.mInput = this.shadowRoot.querySelector('.row__input');
+        this.mInput = this.shadowRoot.querySelector('.component__input');
+
+        const asterisk = this.shadowRoot.querySelector('.component__img--required');
+
+        // -----------------------------------------------------
+        // - Add an input event listener, in order to remove the
+        // - Red bordered required highlight, when some content
+        // - is added into the input
+        // -----------------------------------------------------
+
+        this.mInput.addEventListener('input', e => 
+        {
+            if (this.mInput.value.length)
+            {
+                if (this.mInput.classList.contains('notify-required'))
+                {   
+                    this.mInput.classList.remove('notify-required');
+                }
+
+                if (asterisk.style.display !== 'none')
+                {
+                    asterisk.style.display = 'none';
+                }
+            }
+            else
+            {
+                asterisk.style.display = 'initial';
+            }
+        });
     }
 
     /**
@@ -90,6 +89,15 @@ class TextInputRow extends WCBase
     reset()
     {
         this.mInput.value = '';
+    }
+
+    /**
+     * Adds a class into the input, which sets a red border,
+     * In order to display that the input must be filled
+     */
+    notifyRequired()
+    {
+        this.mInput.classList.add('notify-required');
     }
     // ----------------------------------------------
     // - Lifecycle callbacks
