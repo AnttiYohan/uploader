b24ddc630e072bfc8b7e675b3d5eada8fde1b694
Antti Lappalainen <antti@prototyper.tech>
2021-05-13 15:03:37 +0300

    Focus and scrolling ok

--- js/ScrollContainer.js
+++ js/ScrollContainer.js
@@ -4,18 +4,39 @@ import { ContentHeader } from './ContentHeader.js';
 /**
  * 
  */
- class ScrollContainer extends WCBase
- {
-     constructor()
-     {
-         super();
+class ScrollContainer extends WCBase
+{
+    constructor()
+    {
+        // ---------------------------------------------
+        super();
          
-         /**
+        // ---------------------------------------------
+        // -
+        // -
+        // ---------------------------------------------
+
+        this.hasFocus = false;
+
+        this.cursor =
+        {
+            pos: 0,
+            h:   5,
+            y:   0,
+        };
+
+        /**
           * Holds an array of content header title strings
           * @property {String[]}
           */
         this.mContentSet = [];
 
+        /**
+         * Row height
+         */
+        this.rowHeight = 28;
+
+        this.rowIndex = -1;
          // -----------------------------------------------
          // - Setup ShadowDOM: set stylesheet and content
          // - from template 
@@ -35,7 +56,7 @@ import { ContentHeader } from './ContentHeader.js';
             display: flex;
             flex-direction: column;
             width: 100%;
-            min-height: 200px;
+            height: 100px;
             border-radius: 8px;
             border: 1px solid rgba(0, 0, 0, 0.25);
             box-shadow: 0 0 12px -5px rgba(0, 0, 0, 0.25);    
@@ -58,7 +79,9 @@ import { ContentHeader } from './ContentHeader.js';
 
          this.mContainer = this.shadowRoot.querySelector('.container');
 
-         this.shadowRoot.addEventListener('header-removed', e =>
+         this
+            .shadowRoot
+            .addEventListener('header-removed', e =>
          {
             console.log(`ContentBrowser::ScrollContainer -- 'header-removed' event intercepted`);
          }, true);
@@ -72,7 +95,8 @@ import { ContentHeader } from './ContentHeader.js';
 
     clear()
     {
-        this.mContentSet = [];
+        this.rowIndex = -1;
+        this.hasFocus = false;
         deleteChildren( this.mContainer );
     }
 
@@ -81,9 +105,21 @@ import { ContentHeader } from './ContentHeader.js';
         return this.mContainer.children.length;
     }
 
+    blur()
+    {
+        this.hasFocus = false;
+        this.rowIndex = -1;
+    }
+
+    /**
+     * 
+     * 
+     * @param  {ContentHeader} element 
+     * @return {boolean}
+     */
     addElement(element)
     {
-        if (element instanceof ContentHeader)
+        if(element instanceof ContentHeader)
         {
             this.mContainer.appendChild(element);
             return true;
@@ -92,6 +128,10 @@ import { ContentHeader } from './ContentHeader.js';
         return false;
     }
 
+    /**
+     * 
+     * @param {Array<string>} list 
+     */
     pushContent(list)
     {
         this.clear();
@@ -127,8 +167,9 @@ import { ContentHeader } from './ContentHeader.js';
                     actions
                 };
 
-                this.mContainer
-                    .appendChild(new ContentHeader(options));
+                this
+                .mContainer
+                .appendChild( new ContentHeader(options) );
 
                 rowIndex++;
             }
@@ -168,10 +209,6 @@ import { ContentHeader } from './ContentHeader.js';
         return result;
     }
 
-    resetActions()
-    {
-
-    }
     createTestContent(count = 25)
     {
         let i = 0;
@@ -250,6 +287,115 @@ import { ContentHeader } from './ContentHeader.js';
         console.log(`Element created and added, the result: ${result}`);
 
         return result;
+    }
+
+    up()
+    {
+        const rows = this.mContainer.children;
+
+        if ( ! this.hasFocus)
+        {
+            this.rowIndex = rows.length - 1;
+            rows[this.rowIndex].select();
+            const distance = this.rowIndex * this.rowHeight;
+            this.mContainer.scroll({top: distance});
+            this.hasFocus = true;
+            return;
+        }
+
+        rows[this.rowIndex].reject();
+        this.rowIndex--;
+
+        if (this.rowIndex < 0)
+        {
+            this.rowIndex = rows.length - 1;
+            const distance = this.rowIndex * this.rowHeight;
+            this.mContainer.scroll({top: distance});
+            rows[this.rowIndex].select();
+            return;
+        }
+
+        rows[this.rowIndex].select();
+        /*
+        const bounds = this.mContainer.getBoundingClientRect();
+        const rowBounds = rows[this.cursor.pos].getBoundingClientRect();
+        
+        const viewBottom = bounds.top + bounds.height;
+        const rowBottom  = rowBounds.top + rowBounds.height;
+
+        console.log(`ScrollContainer::down() cursor pos: ${this.cursor.pos}`);
+
+        console.log(`Container bottom: ${viewBottom}, pos bottom: ${rowBottom}`);
+
+        const diff = (rowBottom + 4) - viewBottom;
+
+        if (diff > 0)
+        {
+            //this.mContainer.scrollBy({y: diff, behavior: 'smooth'});
+            this.scrollBy(0, diff);
+        }*/
+
+        const cursorTop = this.rowIndex * this.rowHeight;   
+        const viewTop = this.mContainer.scrollTop;
+
+        console.log(`View top: ${viewTop}, cursor top: ${cursorTop}`);
+
+        const diff = cursorTop - viewTop;
+
+        const height = this.mContainer.getBoundingClientRect().height;
+
+        if (diff < 0)
+        {
+            const mod = -diff % this.rowHeight;
+            console.log(`Diff${-diff} mod 28 => ${mod}`);
+            const distance = mod - diff;
+            this.mContainer.scrollTo(0, cursorTop);
+            //this.mContainer.scroll({top: distance, behavior: 'smooth'});
+        }
+
+    }
+
+    down()
+    {
+        const rows = this.mContainer.children;
+        
+        if ( ! this.hasFocus)
+        {
+            this.rowIndex = 0;
+            rows[this.rowIndex].select();
+            this.hasFocus = true;
+            return;
+        }
+
+        rows[this.rowIndex].reject();
+        this.rowIndex++;
+
+        if (this.rowIndex >= this.mContainer.children.length)
+        {
+            this.rowIndex = 0;
+            this.mContainer.scrollTo(0, 0);
+            rows[this.rowIndex].select();
+            return;
+        }
+
+        rows[this.rowIndex].select();
+
+        const bounds = this.mContainer.getBoundingClientRect();
+        const cursorBottom = this.rowIndex * this.rowHeight + this.rowHeight;   
+        const viewBottom = bounds.height;
+
+        console.log(`Container bottom: ${viewBottom}, pos bottom: ${cursorBottom}`);
+
+        const diff = viewBottom - cursorBottom;
+
+        if (diff < 0)
+        {
+            const mod = -diff % this.rowHeight;
+            console.log(`Diff${-diff} mod 28 => ${mod}`);
+            const distance = mod - diff;
+            this.mContainer.scroll({top: distance});
+        }
+
     }
 
     // ----------------------------------------------
