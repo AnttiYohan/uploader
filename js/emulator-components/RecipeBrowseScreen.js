61dadee42c1faf3f93f94bbc38cc8f2fb483a5b4
Antti Lappalainen <antti@prototyper.tech>
2021-07-09 05:41:50 +0300

    Listens to the recipe-search-result event, tries to parse the event detail, in order to get recipes from it

--- js/emulator-components/RecipeBrowseScreen.js
+++ js/emulator-components/RecipeBrowseScreen.js
@@ -26,19 +26,82 @@ class RecipeBrowseScreen extends WCBase
         this.setupTemplate
         (`<link rel='stylesheet' href='assets/css/components.css'>
         <ul class='component'>
-           <li class='component__row'>Recipe 1</li>
-           <li class='component__row'>Recipe 2</li>
         </ul>`);
 
         this.setupStyle
         (`.component {
+           background-color: #eef;
+           border-radius: 2px;
+           border: 1px solid rgba(0,0,0,.25);
+           box-shadow: 0 0 8px -1px rgba(0,0,0,.25);
            width: 300px;
-        }`);
+           display: flex;
+           flex-direction: column;
+        }
+        .recipe__entry {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 48px;
+            box-shadow: inset 0 -5px 20px -5px rgba(0,0,0,.25);
+            border: 1px solid rgba(0,0,0,.15);
+            font-weight: 200;
+            color: #444;
+            font-size: 15px;
+            text-transform: uppercase;
+        }
+        .recipe_entry .thumbnail {
+            justify-self: flex-start;
+            width: 40px;
+            height: 40px;
+            margin: auto;
+            border-radius: 2px;
+        }
+        `);
         this.mList = this.shadowRoot.querySelector('.component');
 
     }
 
+    populateList( result )
+    {
+        if ( ! result ) return;
 
+        let recipes = undefined;
+
+        try {
+
+            recipes = JSON.parse( result );
+
+        }
+        catch ( error ) { return error; }
+
+        if ( recipes && recipes.length )
+        {
+            for ( const entry of recipes )
+            {
+                const { id, title, mediaDto } = entry;
+                if ( ! id || ! title ) continue;
+
+                if ( mediaDto && mediaDto.thumbnail )
+                {
+                    // create image
+                }
+
+                const recipeEntry = newTagClassAttrs
+                ( 
+                    'li', 
+                    'recipe__entry', 
+                    {
+                        'data-title': title,
+                        'data-id': id
+                    }
+                );
+                recipeEntry.textContent = title;
+
+                this.mList.appendChild( recipeEntry );
+            }
+        }
+    }
     // ----------------------------------------------
     // - Lifecycle callbacks
     // ----------------------------------------------
@@ -50,6 +113,7 @@ class RecipeBrowseScreen extends WCBase
         {
             const result = e.detail.result;
 
+            this.populateList( result );
         }, true);
     }
 
