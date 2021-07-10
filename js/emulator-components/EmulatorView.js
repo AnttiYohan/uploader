741f5a7bb4dd492431a335b56c7eafc4621a5f26
Antti Lappalainen <antti@prototyper.tech>
2021-07-10 21:27:21 +0300

    Refresh button removed, replaced with layout controls

--- js/emulator-components/EmulatorView.js
+++ js/emulator-components/EmulatorView.js
@@ -6,6 +6,7 @@ import { ResponseNotifier } from '../ResponseNotifier.js';
 import { RecipeSearchScreen } from './RecipeSearchScreen.js';
 import { RecipeBrowseScreen } from './RecipeBrowseScreen.js';
 import { RecipeDetailScreen } from './RecipeDetailScreen.js';
+import { EmulatorLayoutControls } from './EmulatorLayoutControls.js';
 
 
 /**
@@ -34,7 +35,7 @@ class EmulatorView extends WCBase
         (`<link rel='stylesheet' href='assets/css/components.css'>
         <div class='uploader'>
             <div class='uploader__frame' data-input-frame>
-                <button class='button--refresh'>Refresh</button>
+                <emulator-layout-controls></emulator-layout-controls>
                 <div class='component__frame'>
                   <recipe-search-screen></recipe-search-screen>
                   <recipe-browse-screen></recipe-browse-screen>
@@ -56,41 +57,6 @@ class EmulatorView extends WCBase
 
         this.mRootElement = this.shadowRoot.querySelector('.uploader');
         
-        const refreshButton = this.shadowRoot.querySelector('.button--refresh');
-        refreshButton.addEventListener
-        ('click', e => 
-        { 
-            //FileCache.clearCache( RECIPE_URL );
-            //this.loadRecipes();
-        });
-
-    }
-
-    /**
-     * Read recipes from cache or from server
-     */
-    loadRecipes()
-    {
-        FileCache.getRequest( RECIPE_URL)
-        .then(response => 
-        { 
-            console.log(`GET /RECIPE_URL (emulator) status: ${response.status}`);
-
-            if (response.ok)
-            {
-                console.log(`Recipes retrieved succesfully`);
-                try { this.generateList( JSON.parse(response.text)); } 
-                catch (error) { throw new Error(`Product parse failed: ${error}`); }
-            }
-            else throw new Error(`Server responded with: ${response.text}`);
-            
-        })
-        .catch(error => { console.log(`Could not read products: ${error}`); });
-    }
-
-    generateList( recipes )
-    {
-        console.log( `Recipe amount: ${recipes.length}` );
     }
 
     // ----------------------------------------------
