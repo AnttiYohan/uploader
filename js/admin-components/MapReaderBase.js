3129691c5b7d065d2638ff2f4218f73106b05934
Antti Lappalainen <antti@prototyper.tech>
2021-08-19 21:17:01 +0300

    Initial commit, these classes parses DTOs and formats the data into a human readable HTMLDocument fragment

---
+++ js/admin-components/MapReaderBase.js
@@ -0,0 +1,407 @@
+import { imgClassSrc, newTagClassChildren, newTagClassHTML } from "../util/elemfactory.js";
+
+/**
+ * MapReaderBase is a DTO parser base object, which is
+ * intended to be extended.
+ * The extending class provides the DTO model in
+ * Which are used to build by extension, for instance
+ * ProductClaimsReader, which reads a product claims response.
+ * The Readers are intended to parse one entity,
+ * If an array of entities are expected, iterate through the 
+ * entities, and parse the DTOs with the reader one itaration at time
+ */
+class MapReaderBase
+{
+    /**
+     * The child class injects a model map
+     * into the the constructor
+     * 
+     * @param {Map} model 
+     *
+    constructor()
+    {
+    }*/
+
+    set model( model )
+    {
+        this.mModel = model;
+    }
+
+    /**
+     * Parses the dto using the model
+     * 
+     */
+    parse()
+    {
+        /**
+         * Stores the entries here,
+         * entry is a HTMLDivElement,
+         * which contains the detailed data
+         * from a DTO entry.
+         * If the entry contains children,
+         * their data is generated also in the entry.
+         */
+        const entries = [];
+
+        for ( const [ key, actions ] of this.mModel )
+        {
+            if ( actions.get( 'type' ) === 'image' )
+            {
+                entries.push( this.getEntityImageEntry( actions ) );
+                continue;
+            }
+            else
+            if ( actions.get( 'type' ) === 'object' && actions.has( 'child_keys' ) )
+            {
+                /**
+                 * Resolve the entry's children, which are an array of objects.
+                 * Store the result, generated HTMLElement, into the
+                 * 'entries' array
+                 */
+                entries.push( this.getEntryFromChildObjects( key, actions ) );
+                continue;
+            }
+            else 
+            if ( actions.get( 'type' ) === 'array' && actions.get( 'value' ).length )
+            {
+                /**
+                 * Resolve the entry's children, which are an array of primitives.
+                 * Store the result, generated HTMLElement, into the
+                 * 'entries' array
+                 */
+                entries.push( this.getEntryFromArray( key, actions ) );
+                continue;
+            }
+
+            /**
+             * Create a html element which contains the
+             * entry 'key' and 'value'
+             */
+            entries.push( newTagClassChildren(
+
+                'h3',
+                'report__entry--simple',
+                [
+                    newTagClassHTML( 'span', 'report__span', key ),
+                    newTagClassHTML( 'span', 'report__span', actions.get( 'value' ) )
+                ]
+
+            ));    
+        }
+
+        return newTagClassChildren(
+
+            'div',
+            'report',
+            entries
+
+        );
+    }
+
+    /**
+     * Creates a HTMLElelemet struct with:
+     * - image title
+     * - image element | paragraph with error message
+     * 
+     * @param  {Map}         actions 
+     * @return {HTMLElement} 
+     */
+    getEntityImageEntry( actions )
+    {
+        return newTagClassChildren(
+
+            'div',
+            'report__entry--image',
+            [
+                newTagClassHTML( 'h3', 'report__header', actions.get( 'title' ) ),
+                ! actions.get( 'data' )
+                ? newTagClassHTML
+                ( 
+                    'h4', 
+                    'report__header',
+                    'Image data not available'
+                )
+                : imgClassSrc
+                ( 
+                    'report__dto-image', 
+                    `data:${actions.get( 'fileType' )};base64,${actions.get( 'data' )}`
+                )
+            ]
+        );    
+    }
+    /**
+     * Generates a HTMLElement structure with
+     * a container and child values,
+     * The data is derived from a DTO entry,
+     * which has an array of primitives
+     * 
+     * @param  {string}      title
+     * @param  {Map}         actions 
+     * @return {HTMLElement} 
+     */
+    getEntryFromArray( title, actions )
+    {
+        /**
+         * Iterate throuhg the child entities,
+         * and fetch the values from array
+         */
+        const childParagraphs = [];
+        
+        for ( const primitive of actions.get( 'value' ) )
+        {
+            const spans = [];
+            spans.push( newTagClassHTML(
+
+                 'span', 
+                 'report__span--child', 
+                 actions.get( 'singular' )
+
+            ));
+
+            spans.push( newTagClassHTML(
+
+                'span', 
+                'report__span--child', 
+                `${primitive}`
+
+           ));
+
+            childParagraphs.push( newTagClassChildren(
+
+                'p',
+                'report__entry--child',
+                spans
+
+            ));
+        }
+
+        return newTagClassChildren(
+
+            'div',
+            'report__entry--deep',
+            [
+                newTagClassHTML( 'h3', 'report__header', title ),
+                ...childParagraphs
+            ]
+        ); 
+    }
+
+    /**
+     * Generates a HTMLElement structure with
+     * a container and child elements,
+     * that contains the details of an DTO key entry,
+     * which has an array of child objects
+     * 
+     * @param  {string}      title
+     * @param  {Map}         actions 
+     * @return {HTMLElement} 
+     */
+    getEntryFromChildObjects( title, actions )
+    {
+        /**
+         * Iterate throuhg the child entities,
+         * and get fetch the entries by 'child_keys'
+         */
+        
+        const childParagraphs = [];
+        
+        for ( const entity of actions.get( 'value' ) )
+        {
+            const spans = [];
+            spans.push( newTagClassHTML(
+
+                'span', 
+                'report__span--child', 
+                actions.get( 'singular' )
+
+            ));
+
+            for ( const key of actions.get( 'child_keys' ) )
+            {
+                if ( key === 'mediaDto' )
+                {
+                    /**
+                     * Generate an inline thumbnail
+                     */
+                    const image = MapReaderBase.getImageData
+                    ( 
+                        `${actions.get( 'singular' )} image`,
+                         entity[ key ]
+                    );
+
+                    spans.push( 
+                        
+                        ! image.get( 'data' )
+                        ? newTagClassHTML( 'span', 'report__span--child', 'Image N/A' )
+                        : imgClassSrc(
+
+                            'report__image--thumbnail',
+                            `data:${image.get( 'fileType' )};base64,${image.get( 'data' )}`                            
+
+                    ));
+                    continue;
+                }
+
+                spans.push( newTagClassHTML(
+
+                    'span',
+                    'report__span--child',
+                    `${key}: ${entity[ key ]}`
+
+                ));
+            }
+
+            childParagraphs.push( newTagClassChildren(
+
+                'p',
+                'report__entry--child',
+                spans
+
+            ));
+        }
+
+        return newTagClassChildren(
+
+            'div',
+            'report__entry--deep',
+            [
+                newTagClassHTML( 'h3', 'report__header', title ),
+                ...childParagraphs
+            ]
+            
+        ); 
+    }
+ 
+    /**
+     * Parses and returns either
+     * the main image, or thumbnali source
+     * as base64 encoded string.
+     * 
+     * The function tries first to read the 
+     * main image, if it is not present,
+     * it tries to read the image thumbnail.
+     * 
+     * @param  {MediaDto} mediaDto 
+     * @return {string}   base64encoded string, the image source 
+     */
+    static imageFromMediaDto( mediaDto )
+    {
+        let image = mediaDto.image;
+        if ( ! image )
+        {
+            image = mediaDto.thumnail;
+        }
+
+        return image;
+    }
+
+    /**
+     * Determines and returns the string representation
+     * of the type of the 
+     * passed param 'value'.
+     * 
+     * @param  {any}       value 
+     * @return {string}    type
+     */
+    static getValueType( value )
+    {
+        let   type    = typeof value;
+        
+        /**
+         * Determine some special cases for type:
+         * - check for array, if type == 'object'
+         * - check for numberic string, if type == 'string'
+         */
+        if ( type === 'object' )
+        {
+            if ( Array.isArray( value ) )
+            {
+                type = 'array';
+            }
+            else
+            if ( value === null )
+            {
+                type = 'null';
+            }
+        }
+        else
+        if ( type === 'string' && ! isNaN( value ) )
+        {
+            type = 'number';
+        }
+
+        return type;
+    }
+
+    /**
+     * Obtains the image source and image filetype
+     * from a 'MediaDto'
+     * 
+     * @param  {MediaDto} mediaDto 
+     * @return {Map}      image data base64, image format
+     */
+    static getImageData( title, mediaDto )
+    {
+        const fileEntity = MapReaderBase.imageFromMediaDto( mediaDto );
+
+        if ( fileEntity && fileEntity.data && fileEntity.fileType )
+        {
+            const result = new Map();
+
+            result.set( 'type', 'image' );
+            result.set( 'title', title );
+            result.set( 'data', fileEntity.data );
+            result.set( 'fileType', fileEntity.fileType );
+
+            return result;
+        }
+
+        /**
+         * There were no image data available,
+         * return a map with 
+         * type->image
+         * data->null
+         */
+        const result = new Map();
+
+        result.set( 'type', 'image' );
+        result.set( 'data', null );
+        result.set( 'title', title );
+
+        return result;
+    }
+
+    static getActions( value )
+    {
+        const type    = MapReaderBase.getValueType( value );
+        const actions = new Map();
+        actions.set( 'type', type );
+        actions.set( 'value', value );
+        
+        return actions;
+    }
+
+    static getObjectActions( singular, childKeys, value )
+    {
+        const actions = new Map();
+        actions.set( 'type', 'object' );
+        actions.set( 'value', value );
+        actions.set( 'singular', singular );
+        actions.set( 'child_keys', childKeys );
+        
+        return actions;
+    }
+
+    static getArrayActions( singular, arrayElemType, value )
+    {
+        const actions = new Map();
+        actions.set( 'type', 'array' );
+        actions.set( 'value', value );
+        actions.set( 'singular', singular );
+        actions.set( 'array_elem_type', arrayElemType );
+
+        return actions;
+    }
+}
+
+export { MapReaderBase };
---
+++ js/admin-components/RecipeReader.js
@@ -0,0 +1,112 @@
+import { MapReaderBase } from './MapReaderBase.js';
+
+/**
+ * RecipeReader is a parser/formatter, intended to
+ * generate a 'human readable' representation of
+ * a 'RecipeDTO' object.
+ * The representation is generated in a HTMLElement structure.
+ * 
+ * The HTMLElements are generated in this format: 
+ * ----------------------------------------------
+ * <div class='report'> <!-- the root node, which contains every other element -->
+ *   <!-- A simple, shallow entry with a primitive value -->
+ *   <h3 class='report__entry--simple'>
+ *     <span class='report__span'>{dto key}</span>
+ *     <span class='report__span'>{dto value}</span>
+ *   </h3>
+ *   <!-- An entry with child data, value is an object or array with child entries -->
+ *   <div class='report__entry--deep'>
+ *     <!-- Entry header, displays the dto key here -->
+ *     <h3 class='report__header'>{dto key}</h3>
+ *     <!-- Entry's child entries from an array of objects -->
+ *     <p class='report__entry--child'>
+ *       <!-- a header span, displaying the title of this child entry -->
+ *       <span class='report__span--header'>{child-title}</span>
+ *       <!-- 1..N spans, displaying the child entry's key/value pair(s) -->
+ *       <span class='report__span--child'>{child entry key}: {child entry value}</span>
+ *       ... rest of the child entry's key/value pair span sets
+ *     </p>
+ *     ... rest of the dto entry's child entries
+ *   </div>
+ *   ... rest of the dto entries
+ * </div>
+ * ---------------------------------------------- 
+ * 
+ */
+class RecipeReader extends MapReaderBase
+{
+    constructor( dto )
+    {
+        super();
+        /**
+        * Define the recipe model
+        */
+        const model = new Map();
+
+        /**
+         * Iterate through the recipe DTO,
+         * add entries to the map, using 
+         * 'key', the value is an object
+         * with certain key-value pairs,
+         * which contain specific values
+         * to tell the MapReaderBase,
+         * which data will be rendered,
+         * and in what fashion.
+         */
+        for ( const key in dto )
+        {
+            const value   = dto[ key ];
+
+            /**
+             * Handle the special cases with a value type
+             * of 'object' and 'array'
+             * Following keys contains these:
+             * - 'products'
+             * - 'steps'
+             * - 'mealTypes'
+             * - 'seasons'
+             * - 'tips' 
+             */
+            if ( key === 'mediaDto' )
+            {
+                model.set( key, MapReaderBase.getImageData( 'Recipe Image', value ) );
+            }
+            else
+            if ( key === 'products' )
+            {
+                model.set( key, MapReaderBase.getObjectActions( 'product', [ 'name', 'id' ], value ) );
+            }
+            else
+            if ( key === 'steps' )
+            {
+                model.set( key, MapReaderBase.getObjectActions( 'step', [ 'stepNumber', 'text', 'mediaDto' ], value ) );
+            }
+            else
+            if ( key === 'mealTypes' )
+            {
+                model.set( key, MapReaderBase.getObjectActions( 'meal type', [ 'name' ], value ) );
+            }
+            else
+            if ( key === 'season' )
+            {
+                model.set( key, MapReaderBase.getObjectActions( 'season', [ 'name' ], value ) );
+            }
+            else
+            if ( key === 'tips' )
+            {
+                model.set( key, MapReaderBase.getArrayActions( 'tip', 'string', value ) );
+            }
+            else
+            {
+                /**
+                 * For primitives, we need value and type
+                 */
+                model.set( key, MapReaderBase.getActions( value ) );
+            }    
+        }
+
+        this.model = model;
+    }
+}
+
+export { RecipeReader };
