dd2e6573a03d03579abdda6afe09952ff133c1ac
Antti Lappalainen <antti@prototyper.tech>
2021-08-20 20:07:12 +0300

    Layout updated into link/detail two column view

--- js/AdminView.js
+++ js/AdminView.js
@@ -1,11 +1,6 @@
 import { WCBase } from './WCBase.js';
-import { StatSet } from './admin-components/StatSet.js';
-import { FileCache } from './util/FileCache.js';
-import { EntryBrowser } from './EntryBrowser.js';
-import { TextInputRow } from './TextInputRow.js';
-import { ImageInputRow } from './ImageInputRow.js';
-import { InputOperator } from './util/InputOperator.js';
-import { ResponseNotifier } from './ResponseNotifier.js';
+import { LinkMenu } from './admin-components/LinkMenu.js';
+import { deleteChildren } from './util/elemfactory.js';
 
 /**
  * AdminView is one of the Top Level Views in the
@@ -29,19 +24,78 @@ class AdminView extends WCBase
         this.setupTemplate
         (`<link rel='stylesheet' href='assets/css/components.css'>
          <div class='admin'>
-           <stat-set data-entities='products'
-                     data-entity='product' 
-                     data-route='product/claims'
-                     data-related='claims'
-                     data-reader='ProductClaimsReader'
-                     data-relation-phrase='is claimed by'       
-            >Products used by Recipes</stat-set>
-         </div>`);    
+            <link-menu data-entries='[
+                {
+                    "icon": "assets/icon_edit.svg",
+                    "header": "product claims",
+                    "link": "StatSet",
+                    "params" : [ 
+                        "product/claims", 
+                        "products", 
+                        "ProductClaimsReader" 
+                    ]
+                },
+                {
+                    "icon": "assets/icon_placeholder.svg",
+                    "header": "media",
+                    "link": "ImageManager"
+                }
+            ]'></link-menu>
+            <div class='admin__detail'>
+            </div>`);    
+        
+        const offsetTop = this.offsetTop;
         
         this.setupStyle
-        (`.admin { margin: 0 auto; display: flex; flex-direction: column; min-width: 300px; width: 85vw; max-width: 1200px;}`);
-        
+        (`.admin { 
+            margin: 0 auto; 
+            display: flex; 
+            flex-direction: row;  
+            max-width: 1200px;
+            height: calc(100vh - 57px);
+        }
+        .admin__detail {
+            overflow-y: scroll;
+        }`);
+
+        this.mRootElement = this.shadowRoot.querySelector( '.admin' );
+        this.mDetailView  = this.shadowRoot.querySelector( '.admin__detail' );
     }  
+
+    /**
+     * Opens a linked view into the DetailView
+     * 
+     * @param {string} link 
+     */
+    async openDetail( initiator )
+    {
+        if ( typeof initiator.link !== 'string' || initiator.link.length < 4 ) return;
+
+        const link      = initiator.link;
+        const params    = Array.isArray( initiator.params )
+                        ? initiator.params
+                        : [];
+
+        const path      = './admin-components/';
+        const linkClass = ( await import( `${path}${link}.js` ) )[ link ];
+        const element   = new linkClass( ...params );
+
+        if ( element instanceof HTMLElement )
+        {
+            deleteChildren( this.mDetailView );
+            this.mDetailView.appendChild( element );
+        }   
+    }
+
+    connectedCallback()
+    {
+        const offsetTop = this.offsetTop;
+        if ( offsetTop )
+        {
+            this.mRootElement.style.height = `calc(100vh - ${offsetTop}px)`;
+        }
+        this.listen( 'link-open', e => this.openDetail( e.detail ) );
+    }
 }
 
 window.customElements.define( 'admin-view', AdminView );
